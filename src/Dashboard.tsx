94318b5948cecf06f50d2e9509cd041513abd3e4
FlorentCollin <florent.collin23@gmail.com>
2020-07-14 10:23:57 +0200

    Add more comments for an easier understanding

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -8,11 +8,18 @@ const Dashboard: React.FC = () => {
     const webId = useWebId();
     const [name, setName] = useState<string>('undefined');
     const [appContainer, setAppContainer] = useState<TripleDocument>();
+    // useEffect is triggered when the state of this component
     useEffect(() => {
-        const getName = async (webId: string) => {
-            const profileDoc = await fetchDocument(webId);
-            const profile = profileDoc.getSubject(webId);
-            setName(profile.getString(foaf.name) || 'undefined');
+        const getName = async (webID: string) => {
+            // Fetch the profile document on the solid pod
+            const profileDoc = await fetchDocument(webID);
+            const profile = profileDoc.getSubject(webID);
+            // This try to fetch the name of the Solid Pod user
+            // If there is no name in the profile document we use the webID as name
+            setName(profile.getString(foaf.name) || webID);
+            // Create the application folder
+            // The appContainer must be use as a base path for each form submitted
+            // to keep all the forms inside the same folder
             const appContainer = await initializeDataFolder(profile);
             if (appContainer != null) {
                 setAppContainer(appContainer);
@@ -29,10 +36,13 @@ const Dashboard: React.FC = () => {
         <>
             <h1>You are connected 🎉</h1>
             <p>Welcome back, {name}</p>
+            <p>Open you dev console to see logs</p>
+            <p>And check the folder /public/solidelections in you solid pod</p>
             <button onClick={() => {
                 if (appContainer) {
                     addDummyText(appContainer);
                 }
+                console.log("The dummy person file has been added to /public/solidelections/dummyPerson.ttl")
             }}>Add a dummy Person</button>
         </>
     );
--- src/SolidWrapper.ts
+++ src/SolidWrapper.ts
@@ -8,16 +8,17 @@ import auth from 'solid-auth-client';
 // This `TripleDocument` is going to be use to store every form submitted by a user.
 // This function must be called just after the authentication of the user.
 export async function initializeDataFolder(profile: TripleSubject): Promise<TripleDocument | null> {
-    //Decide at what URL within the user's Pod the new data folder should be stored
+    const appName = 'solidelections';
+    // Fetch the Public Type Index. This document is where we can "link" the app to an actual folder inside the solid pod
+    // By default the public type index is on `settings/publicTypeIndex.ttl`
     const publicTypeIndex = await fetchPublicTypeIndex(profile);
     if (publicTypeIndex == null) {
         console.error("Public Type Index was not found inside the user profile");
         return null;
     }
     //Check if an app folder (named container in solid documentation) as already been made.
-    const appRef = `${publicTypeIndex.asRef()}#solidelections`;
-    const appSubject = publicTypeIndex.findSubject(rdf.type, solid.TypeRegistration);
-    if (appSubject != null && appSubject.asRef() === appRef) {
+    const appSubject = await findAppSubject(publicTypeIndex, appName);
+    if (appSubject) {
         // The app folder has already been made
         console.log("initializeDataFolder: The application folder was already created")
         // Get the reference to the location of the app folder
@@ -31,7 +32,7 @@ export async function initializeDataFolder(profile: TripleSubject): Promise<Trip
 
     // The app folder has not been made yet
     // Therefore we can create it.
-    const defaultPath = profile.getRef(space.storage) + 'public/solidelections';
+    const defaultPath = profile.getRef(space.storage) + `public/${appName}`;
     const appContainer = await createAppFolder(defaultPath);
 
     // Store a reference to the app folder in the public type index
@@ -60,6 +61,22 @@ async function fetchPublicTypeIndex(profile: TripleSubject): Promise<TripleDocum
     return publicTypeIndexRef ? await fetchDocument(publicTypeIndexRef) : null
 }
 
+async function findAppSubject(publicTypeIndex: TripleDocument, appName: string): Promise<TripleSubject | null> {
+    const appRef = `${publicTypeIndex.asRef()}#${appName}`;
+
+    // Remember that everything is store as Triples (Subject - Predicate - Object).
+    // Here we try to find the subject name that correspond to our application
+    // There may be multiple apps already registered in the public type index
+    // We need to check if our app is already registered or not.
+    const appSubjects = publicTypeIndex.findSubjects(rdf.type, solid.TypeRegistration);
+    for (const subject of appSubjects) {
+        if (subject.asRef() === appRef) {
+            return subject;
+        }
+    }
+    return null;
+}
+
 async function createAppFolder(appFolderPath: string): Promise<TripleDocument> {
     const split = appFolderPath.lastIndexOf('/');
     const parentFolderPath = appFolderPath.substr(0, split);
@@ -76,5 +93,3 @@ async function createAppFolder(appFolderPath: string): Promise<TripleDocument> {
     }).catch(error => console.log('Error: ' + JSON.stringify(error)));
     return fetchDocument(appFolderPath);
 }
-
-
